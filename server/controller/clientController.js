c207d45eb3c77573daf461094c77de6fbd582156
rob587 <robertocamm02@yahoo.com>
2026-04-28 22:47:29 +0200

    feat: implement create and update client functionalities in client controller

--- server/controller/clientController.js
+++ server/controller/clientController.js
@@ -9,3 +9,34 @@ exports.getClients = (req, res) => {
     res.json(results);
   });
 };
+
+// nuovo cliente
+
+exports.createClient = (req, res) => {
+  const { name, email, phone } = req.body;
+
+  const sql = "INSERT INTO clients (name, email,phone) VALUES (?, ?, ?)";
+
+  db.query(sql, [name, email, phone], (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json({ message: "Client added", id: result.insertId });
+  });
+};
+
+// AGGIORNA cliente
+
+exports.updateClient = (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+
+  const sql = "UPDATE clients SET name=?, email=?, phone=? WHERE id=?";
+
+  db.query(sql, [name, email, phone, id], (err) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json({ message: "Client updated" });
+  });
+};
