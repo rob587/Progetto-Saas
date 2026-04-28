a9aaad754e2284fc944203929765f4036d7eb7c7
rob587 <robertocamm02@yahoo.com>
2026-04-28 23:18:32 +0200

    feat: implement user registration functionality in auth controller and add necessary dependencies

---
+++ server/controller/authControllers.js
@@ -0,0 +1,41 @@
+const db = require("../db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTRAZIONE
+
+exports.register = (req, res) => {
+  const { username, email, password } = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE username = ? OR email = ?",
+    [username, email],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      if (results.length > 0) {
+        return res
+          .status(400)
+          .json({ message: "Username or email already exists" });
+      }
+      bcrypt.hash(password, 8, (err, hashedPassword) => {
+        if (err) {
+          return res.status(500).json(err);
+        }
+        const sql =
+          "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+        db.query(sql, [username, email, hashedPassword], (err, result) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
+          res.json({
+            message: "User registered successfully",
+            userId: result.insertId,
+          });
+        });
+      });
+    },
+  );
+};
