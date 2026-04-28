9b2e61bba5c0f502e3bff4470af5996741f6db05
rob587 <robertocamm02@yahoo.com>
2026-04-28 23:34:45 +0200

    feat: implement login functionality in auth controller

--- server/controller/authControllers.js
+++ server/controller/authControllers.js
@@ -39,3 +39,26 @@ exports.register = (req, res) => {
     },
   );
 };
+
+// LOGIN
+
+exports.login = (req, res) => {
+  const { username, password } = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      if (results.length === 0) {
+        return res
+          .status(401)
+          .json({ message: "Username or password is incorrect" });
+      }
+
+      const user = results[0];
+    },
+  );
+};
