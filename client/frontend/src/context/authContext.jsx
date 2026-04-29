6e7702fa344d38f458ee149fb45145647b2d7912
rob587 <robertocamm02@yahoo.com>
2026-04-29 14:15:23 +0200

    feat: implement login and logout functions with token management in auth context

--- client/frontend/src/context/authContext.jsx
+++ client/frontend/src/context/authContext.jsx
@@ -5,4 +5,26 @@ export const authContext = createContext();
 export const authProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem("token"));
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    }
+  }, [token]);
+
+  const login = (newToken) => {
+    setToken(token);
+    localStorage.setItem("token", newToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return(
+    <authContext.Provider value={user, token, login, logout}>
+        {children}
+    </authContext.Provider>
+  )
 };
--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { authProvider } from "./context/authContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <authProvider>
+      <App />
+    </authProvider>
   </StrictMode>,
 );
