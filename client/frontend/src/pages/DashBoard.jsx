f3555671cb64be39b962a6470ca3129dbb8aea92
rob587 <robertocamm02@yahoo.com>
2026-04-30 14:00:13 +0200

    feat: set up initial state and context for DashBoard component

--- client/frontend/src/pages/DashBoard.jsx
+++ client/frontend/src/pages/DashBoard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import API from "../services/api";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 
 const DashBoard = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+  const [clients, setClients] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  return <></>;
 };
 
 export default DashBoard;
