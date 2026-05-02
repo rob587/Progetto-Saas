c0e6627d7466f1270d2625ce5bfe920a74771bba
rob587 <robertocamm02@yahoo.com>
2026-05-02 17:33:28 +0200

    feat: implement initial state and context for Register component

--- client/frontend/src/pages/Register.jsx
+++ client/frontend/src/pages/Register.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  return <></>;
 };
 
 export default Register;
