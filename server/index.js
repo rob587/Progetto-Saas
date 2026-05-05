750cf64c683f842f20872656bd542ab8921523d1
rob587 <robertocamm02@yahoo.com>
2026-05-05 13:27:54 +0200

    add CORS for production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ const db = require("./db");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://progetto-saas.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
