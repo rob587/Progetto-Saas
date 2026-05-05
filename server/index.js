688e96383b5396fe1663f971bb56bf465eb5349c
rob587 <robertocamm02@yahoo.com>
2026-05-05 23:01:39 +0200

    simplify cors

--- server/index.js
+++ server/index.js
@@ -5,26 +5,7 @@ const db = require("./db");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "https://progetto-saas.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:5173",
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 // Routes
