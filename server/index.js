1508e5a008fca08a2a83aba0db65f84e8131b948
rob587 <robertocamm02@yahoo.com>
2026-05-05 23:24:28 +0200

    fix: update server listen address to 0.0.0.0 for external access

--- server/index.js
+++ server/index.js
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 console.log(`⏳ Attempting to listen on port ${PORT}`);
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on 0.0.0.0:${PORT}`);
 });
