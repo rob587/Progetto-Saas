7d9737a4dcb460903d9b5ae5067003817abc3b21
rob587 <robertocamm02@yahoo.com>
2026-05-05 13:07:23 +0200

    backend ready for deployment

--- server/db.js
+++ server/db.js
@@ -12,7 +12,7 @@ db.connect((err) => {
   if (err) {
     console.error("DB connection error:", err);
   } else {
-    console.log("✅ Connected to MySQL");
+    console.log("✅ Connected to MySQL DB");
   }
 });
 
